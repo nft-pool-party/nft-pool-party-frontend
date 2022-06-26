d4c28128cf7d21cc5e59816a929f631074e6b7e9
varunv5000 <varunv5000@gmail.com>
2022-06-26 08:11:15 -0400

    Update getNFTs.js

--- src/getNFTs.js
+++ src/getNFTs.js
@@ -5,12 +5,12 @@ export default function getNFTs(address, setNFTs){
 
 const baseURL = "https://eth-mainnet.alchemyapi.io/nft/v2/hA2ONXLuakmXgxSTdhzyGU3-hZC6339Z/getNFTs/";
 const ownerAddr = address;
-const contractAddrs = ["0x1485297e942ce64E0870EcE60179dFda34b4C625", "0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e", "0x1A92f7381B9F03921564a437210bB9396471050C"];
+const contractAddrs = ["0x41f20599e9e049004C4d169046eb7023117a6244", "0x1A92f7381B9F03921564a437210bB9396471050C"];
 
 
 var config = {
   method: 'get',
-  url: `${baseURL}?owner=${ownerAddr}&contractAddresses[]=${contractAddrs[0]}&contractAddresses[]=${contractAddrs[1]}&contractAddresses[]=${contractAddrs[2]}`
+  url: `${baseURL}?owner=${ownerAddr}&contractAddresses[]=${contractAddrs[0]}&contractAddresses[]=${contractAddrs[1]}`
 };
 
 axios(config)
